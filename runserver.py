587d70744d8454a578c16ee673658fbe8da72255
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-11-28 18:56:12 +0100

    Add the runserver file

---
+++ runserver.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" The flask application """
+
+## These two lines are needed to run on EL6
+__requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
+import pkg_resources
+
+from cnucnuweb.app import APP
+
+if __name__ == '__main__':
+    APP.debug = True
+    APP.run()
