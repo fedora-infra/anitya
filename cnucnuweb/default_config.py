587e125c7871cf6199d65fa1c08bec04e815d501
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-11-28 18:55:15 +0100

    Add the default config

---
+++ cnucnuweb/default_config.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'changeme please'
+
+DB_URL = 'sqlite:////var/tmp/cnucnu-web-dev.sqlite'
+
+CNUCNU_WEB_ADMINS = ['ralph.id.fedoraproject.org']
+
+CNUCNU_WEB_FEDORA_OPENID = 'https://id.fedoraproject.org'
+
+CNUCNU_WEB_ALLOW_FAS_OPENID = True
+CNUCNU_WEB_ALLOW_GOOGLE_OPENID = True
+CNUCNU_WEB_ALLOW_YAHOO_OPENID = True
+CNUCNU_WEB_ALLOW_GENERIC_OPENID = True
