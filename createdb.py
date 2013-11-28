bb05305e9f6c8cacacea5e4ee6c04fe720ee9c1f
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-11-28 18:56:06 +0100

    Add the createdb file

---
+++ createdb.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+## These two lines are needed to run on EL6
+__requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
+import pkg_resources
+
+from cnucnuweb.app import APP
+from cnucnuweb import model
+
+model.init(
+    APP.config['DB_URL'],
+    None,
+    debug=True,
+    create=True)
