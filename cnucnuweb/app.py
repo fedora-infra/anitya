7bb0fcb8ac4e0803d7ad2cb4d73949a9ade42ecf
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-12-01 16:58:37 +0100

    Add method to check the version retrieved by cnucnu on the fly
    Using the check button one can check which version cnucnu retrieves with
    the information provided.

--- cnucnuweb/app.py
+++ cnucnuweb/app.py
@@ -16,6 +16,8 @@ import sqlalchemy
 from bunch import Bunch
 from flask.ext.openid import OpenID
 
+from cnucnu.package_list import Package
+
 import cnucnuweb.model
 import cnucnuweb.forms
 
@@ -206,6 +208,49 @@ def api_projects():
             "\n".join(projects),
             content_type="text/plain;charset=UTF-8"
         )
+
+
+@APP.route('/api/versions/', methods=['POST'])
+def api_get_version():
+
+    name = flask.request.form.get('name', None)
+    url = flask.request.form.get('url', None)
+    regex = flask.request.form.get('regex', None)
+    httpcode = 200
+
+    if not name or not url or not regex:
+        errors = []
+        if not name:
+            errors.append('No name specified')
+        if not url:
+            errors.append('No url specified')
+        if not regex:
+            errors.append('No regex specified')
+        output = {'output': 'notok', 'error': errors}
+        httpcode = 400
+    else:
+        try:
+            pkg = Package(name=name, url=url, regex=regex)
+            versions = pkg.upstream_versions
+            latest_version = pkg.latest_upstream
+        except Exception as err:
+            output = {'output': 'notok', 'error': err.message}
+            httpcode = 400
+        else:
+            output = {
+                'name': pkg.name,
+                'url': pkg.url,
+                'regex': pkg.regex,
+                'raw_url' : pkg.raw_url,
+                'raw_regex' : pkg.raw_regex,
+                'versions' : versions,
+                'latest_version' : latest_version,
+            }
+
+
+    jsonout = flask.jsonify(output)
+    jsonout.status_code = httpcode
+    return jsonout
 
 
 @APP.route('/projects')
