b7b9c8ffbfc1fcc70132b9a5ac0f756fd106e983
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-12-05 17:37:41 +0100

    Make of Ralph an admin by default

--- cnucnuweb/default_config.py
+++ cnucnuweb/default_config.py
@@ -2,7 +2,9 @@ SECRET_KEY = 'changeme please'
 
 DB_URL = 'sqlite:////var/tmp/cnucnu-web-dev.sqlite'
 
-CNUCNU_WEB_ADMINS = ['ralph.id.fedoraproject.org']
+CNUCNU_WEB_ADMINS = [
+    'http://ralph.id.fedoraproject.org/',
+]
 
 CNUCNU_WEB_FEDORA_OPENID = 'https://id.fedoraproject.org'
 
