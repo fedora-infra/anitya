490e327db3584a44b04988d3642415351518c8b3
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-12-06 13:55:05 +0100

    Add basic log browsing page

--- cnucnuweb/app.py
+++ cnucnuweb/app.py
@@ -574,6 +574,74 @@ def delete_project(project_name):
         form=form)
 
 
+@APP.route('/logs')
+@login_required
+def browse_logs():
+
+    if not admin():
+        flask.abort(403)
+
+    cnt_logs = cnucnuweb.model.Log.search(SESSION, count=True)
+
+    from_date = flask.request.args.get('from_date', None)
+    project = flask.request.args.get('project', None)
+    refresh = flask.request.args.get('refresh', False)
+    limit = flask.request.args.get('limit', 50)
+    page = flask.request.args.get('page', 1)
+
+    try:
+        page = int(page)
+    except ValueError:
+        page = 1
+
+    try:
+        int(limit)
+    except ValueError:
+        limit = 50
+        flask.flash('Incorrect limit provided, using default', 'errors')
+
+    if from_date:
+        try:
+            from_date = parser.parse(from_date)
+        except (ValueError, TypeError):
+            flask.flash(
+                'Incorrect from_date provided, using default', 'errors')
+            from_date = None
+
+    if from_date:
+        from_date = from_date.date()
+
+    if page is not None and limit is not None and limit != 0:
+        page = (page - 1) * limit
+
+    logs = []
+    try:
+        logs = cnucnuweb.model.Log.search(
+            SESSION,
+            project_name=project or None,
+            from_date=from_date,
+            offset=page,
+            limit=limit,
+        )
+    except Exception, err:
+        import logging
+        logging.exception(err)
+        flask.flash(err, 'errors')
+
+    total_page = int(ceil(cnt_logs / float(limit)))
+
+    return flask.render_template(
+        'logs.html',
+        refresh=refresh,
+        logs=logs,
+        cnt_logs=cnt_logs,
+        total_page=total_page,
+        page=page,
+        project=project or '',
+        from_date=from_date or '',
+    )
+
+
 @APP.route('/project/<project_name>/map', methods=['GET', 'POST'])
 @login_required
 def map_project(project_name):
