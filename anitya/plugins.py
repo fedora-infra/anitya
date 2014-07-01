1b2df5ff5cd415becbae2d2c565cc3d98c48bff4
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-07-01 15:28:31 +0200

    Add the plugins module managing the different backends possible

---
+++ anitya/plugins.py
@@ -0,0 +1,48 @@
+#-*- coding: utf-8 -*-
+
+"""
+ (c) 2014 - Copyright Red Hat Inc
+
+ Authors:
+   Pierre-Yves Chibon <pingou@pingoured.fr>
+
+Module handling the load/call of the plugins of anitya
+"""
+
+from sqlalchemy.exc import SQLAlchemyError
+
+import anitya.lib.model as model
+from straight.plugin import load
+from anitya.backends import BaseBackend
+
+
+def load_plugins(session):
+    ''' Load all the plugins and insert them in the database if they are
+    not already present. '''
+    try:
+        backends = model.Backend.all(session)
+    except SQLAlchemyError:
+        return
+
+    plugins = get_plugin_names()
+    for backend in set(backends).symmetric_difference(set(plugins)):
+        bcke = model.Backend(name=backend)
+        session.add(bcke)
+        session.flush()
+    session.commit()
+    return plugins
+
+
+def get_plugin_names():
+    ''' Return the list of plugins names. '''
+    plugins = load('anitya.backends', subclasses=BaseBackend)
+    output = [plugin.name for plugin in plugins]
+    return output
+
+
+def get_plugin(plugin_name):
+    ''' Return the plugin corresponding to the given plugin name. '''
+    plugins = load('anitya.backends', subclasses=BaseBackend)
+    for plugin in plugins:
+        if plugin.name == plugin_name:
+            return plugin
