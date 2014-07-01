fddfeaa8ffa9937a6353fb37f39d2fdc6211e861
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-07-01 15:20:21 +0200

    Adjust the createdb script to work with the new structure

--- createdb.py
+++ createdb.py
@@ -5,10 +5,10 @@
 __requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
 import pkg_resources
 
-from cnucnuweb.app import APP
-from cnucnuweb import model
+from anitya.app import APP
+import anitya.lib
 
-model.init(
+anitya.lib.init(
     APP.config['DB_URL'],
     None,
     debug=True,
