5b6a66722e268505d0936590e37396c311cdfcf0
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-07-07 16:38:14 +0200

    Add backend for project hosted on pecl.php.net

---
+++ anitya/lib/backends/pecl.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+"""
+ (c) 2014 - Copyright Red Hat Inc
+
+ Authors:
+   Pierre-Yves Chibon <pingou@pingoured.fr>
+
+"""
+
+
+from anitya.lib.backends import BaseBackend, get_versions_by_regex
+from anitya.lib.exceptions import AnityaPluginException
+
+REGEX= b'%(name)s[-_]([^-/_\s]+?)(?i)(?:[-_]'\
+    '(?:src|source))?\.(?:tar|t[bglx]z|tbz2|zip)'
+
+
+class PeclBackend(BaseBackend):
+    ''' The custom class for projects hosted on pecl.php.net.
+
+    This backend allows to specify a version_url and a regex that will
+    be used to retrieve the version information.
+    '''
+
+    name = 'Pecl'
+    examples = [
+        'http://pecl.php.net/package/inotify',
+        'http://pecl.php.net/package/gnupg',
+    ]
+
+    @classmethod
+    def get_version(cls, project):
+        ''' Method called to retrieve the latest version of the projects
+        provided, project that relies on the backend of this plugin.
+
+        :arg Project project: a :class:`model.Project` object whose backend
+            corresponds to the current plugin.
+        :return: the latest version found upstream
+        :return type: str
+        :raise AnityaPluginException: a
+            :class:`anitya.lib.exceptions.AnityaPluginException` exception
+            when the version cannot be retrieved correctly
+
+        '''
+        return cls.get_ordered_versions(project)[-1]
+
+    @classmethod
+    def get_versions(cls, project):
+        ''' Method called to retrieve all the versions (that can be found)
+        of the projects provided, project that relies on the backend of
+        this plugin.
+
+        :arg Project project: a :class:`model.Project` object whose backend
+            corresponds to the current plugin.
+        :return: a list of all the possible releases found
+        :return type: list
+        :raise AnityaPluginException: a
+            :class:`anitya.lib.exceptions.AnityaPluginException` exception
+            when the versions cannot be retrieved correctly
+
+        '''
+        url_template = 'http://pecl.php.net/package/%(name)s/download'
+
+        url = url_template % {'name': project.name}
+        regex = REGEX % {'name': project.name}
+
+        try:
+            versions = get_versions_by_regex (url, regex, project)
+        except AnityaPluginException:
+            name = project.name.replace("-", "_")
+            url = url_template % {'name': name}
+            regex = REGEX % {'name': name}
+            versions = get_versions_by_regex (url, regex, project)
+
+        return versions
