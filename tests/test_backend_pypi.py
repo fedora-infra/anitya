653a532227e557edad4e5096086e0703c6e8b3e8
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-07-07 11:36:26 +0200

    Add unit-tests based on chai to test the versions ordering logic

--- tests/test_backend_pypi.py
+++ tests/test_backend_pypi.py
@@ -68,6 +68,14 @@ class PypiBackendtests(Modeltests):
         self.session.add(project)
         self.session.commit()
 
+        project = model.Project(
+            name='chai',
+            homepage='https://pypi.python.org/pypi/chai',
+            backend='pypi',
+        )
+        self.session.add(project)
+        self.session.commit()
+
     def test_pypi_get_version(self):
         """ Test the get_version function of the pypi backend. """
         pid = 1
@@ -84,6 +92,12 @@ class PypiBackendtests(Modeltests):
             project
         )
 
+        pid = 3
+        project = model.Project.get(self.session, pid)
+        exp = '0.4.8'
+        obs = backend.PypiBackend.get_version(project)
+        self.assertEqual(obs, exp)
+
     def test_pypi_get_versions(self):
         """ Test the get_versions function of the pypi backend. """
         pid = 1
@@ -99,6 +113,20 @@ class PypiBackendtests(Modeltests):
             backend.PypiBackend.get_versions,
             project
         )
+
+        pid = 3
+        project = model.Project.get(self.session, pid)
+        exp = [
+            '0.1.0', '0.1.1', '0.1.2', '0.1.3', '0.1.4', '0.1.5', '0.1.6',
+            '0.1.7', '0.1.9', '0.1.10', '0.1.11', '0.1.12', '0.1.13',
+            '0.1.14', '0.1.15', '0.1.16', '0.1.17', '0.1.18', '0.1.19',
+            '0.1.20', '0.1.21', '0.1.22', '0.1.23', '0.2.0', '0.2.1',
+            '0.2.2', '0.2.3', '0.2.4', '0.3.0', '0.3.1', '0.3.2', '0.3.3',
+            '0.3.5', '0.3.6', '0.3.7', '0.4.0', '0.4.1', '0.4.2', '0.4.3',
+            '0.4.4', '0.4.5', '0.4.6', '0.4.7', '0.4.8'
+        ]
+        obs = backend.PypiBackend.get_ordered_versions(project)
+        self.assertEqual(obs, exp)
 
 
 if __name__ == '__main__':
