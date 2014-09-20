f0f310117124a1687dc101fafd33166e068ee2cb
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-09-20 22:49:37 +0200

    Import the requirements in setup.py from the requirements.txt file

--- setup.py
+++ setup.py
@@ -12,6 +12,24 @@ from setuptools import setup
 from anitya.app import __version__
 
 
+def get_requirements(requirements_file='requirements.txt'):
+    """Get the contents of a file listing the requirements.
+
+    :arg requirements_file: path to a requirements file
+    :type requirements_file: string
+    :returns: the list of requirements, or an empty list if
+              `requirements_file` could not be opened or read
+    :return type: list
+    """
+
+    lines = open(requirements_file).readlines()
+    return [
+        line.rstrip().split('#')[0]
+        for line in lines
+        if not line.startswith('#')
+    ]
+
+
 setup(
     name='anitya',
     description='anitya is a project to monitor upstream releases in a distro.',
@@ -26,9 +44,5 @@ setup(
     packages=['anitya'],
     include_package_data=True,
     scripts=['files/anitya_cron.py'],
-    install_requires=[
-        'Flask', 'SQLAlchemy>=0.6', 'wtforms', 'flask-wtf',
-        'python-fedora', 'python-openid', 'straight.plugin',
-        'python-dateutil', 'bunch', 'markupsafe', 'fedmsg'
-    ],
+    install_requires=get_requirements(),
 )
