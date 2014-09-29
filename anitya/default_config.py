a752b3e1d3a09471b2a4d956920cf883c8f0325a
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-09-29 09:27:20 +0200

    Add some documentation to the default configuration file

--- anitya/default_config.py
+++ anitya/default_config.py
@@ -1,14 +1,35 @@
+#-*- coding: utf-8 -*-
+
+"""
+ (c) 2014 - Copyright Red Hat Inc
+
+ Authors:
+   Pierre-Yves Chibon <pingou@pingoured.fr>
+
+anitya default configuration.
+"""
+
+from datetime import timedelta
+
+# Set the time after which the session expires
+PERMANENT_SESSION_LIFETIME = timedelta(hours=1)
+
+# Secret key used to generate the csrf token in the forms
 SECRET_KEY = 'changeme please'
 
+# URL to the database
 DB_URL = 'sqlite:////var/tmp/anitya-dev.sqlite'
 
+# List of admins based on their openid
 CNUCNU_WEB_ADMINS = [
     'http://ralph.id.fedoraproject.org/',
     'http://pingou.id.fedoraproject.org/',
 ]
 
+# Fedora OpenID endpoint
 CNUCNU_WEB_FEDORA_OPENID = 'https://id.fedoraproject.org'
 
+# Booleans to activate or not OpenID providers
 CNUCNU_WEB_ALLOW_FAS_OPENID = True
 CNUCNU_WEB_ALLOW_GOOGLE_OPENID = True
 CNUCNU_WEB_ALLOW_YAHOO_OPENID = True
