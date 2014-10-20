deaec4886ccdf545d2a39999123e23764406c55f
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-10-20 17:54:47 +0200

    Use the REGEX from the backends in the tests, reduces redundancy

--- tests/test_flask_api.py
+++ tests/test_flask_api.py
@@ -36,6 +36,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(
 
 import anitya
 import anitya.lib.model as model
+from anitya.lib.backends import REGEX
 from tests import Modeltests, create_distro, create_project, create_package
 
 
@@ -227,9 +228,7 @@ class AnityaWebAPItests(Modeltests):
             "error": [
                 "geany: no upstream version found. "
                 "- http://www.geany.org/Down - "
-                " geany(?:[-_](?:minsrc|src|source))?[-_]([^-/_\\s]+?)(?i)"
-                "(?:[-_](?:minsrc|src|source))?"
-                "\\.(?:tar|t[bglx]z|tbz2|zip)"
+                " " + REGEX % ({'name': 'geany'})
             ],
             "output": "notok"
         }
