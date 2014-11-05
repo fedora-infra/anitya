e4a64f722291faff2a6e3c4cbc9b07e3092d47ba
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-11-05 11:02:50 +0100

    Add documentation for the current API endpoints

--- anitya/api.py
+++ anitya/api.py
@@ -12,6 +12,60 @@ from anitya.app import APP, SESSION
 @APP.route('/api/projects/')
 @APP.route('/api/projects')
 def api_projects():
+    '''
+List all projects
+-----------------
+    Lists all the projects registered in anitya.
+
+    ::
+
+        /api/projects
+
+        /api/projects/?pattern=<pattern>
+
+    Accept GET queries only.
+
+    :kwarg pattern: pattern to use to restrict the list of names returned.
+
+    Sample response:
+
+    ::
+
+      {
+        "projects": [
+          {
+            "backend": "custom",
+            "created_on": 1409917222.0,
+            "homepage": "http://www.finnie.org/software/2ping/",
+            "id": 2,
+            "name": "2ping",
+            "regex": null,
+            "updated_on": 1414400794.0,
+            "version": "2.1.1",
+            "version_url": "http://www.finnie.org/software/2ping",
+            "versions": [
+              "2.1.1"
+            ]
+          },
+          {
+            "backend": "custom",
+            "created_on": 1409917223.0,
+            "homepage": "http://www.3proxy.ru/download/",
+            "id": 3,
+            "name": "3proxy",
+            "regex": null,
+            "updated_on": 1415115096.0,
+            "version": "0.7.1.1",
+            "version_url": "http://www.3proxy.ru/download/",
+            "versions": [
+              "0.7.1.1"
+            ]
+          }
+        ],
+        "total": 2
+      }
+
+    '''
 
     pattern = flask.request.args.get('pattern', None)
 
@@ -39,6 +93,26 @@ def api_projects():
 @APP.route('/api/projects/wiki/')
 @APP.route('/api/projects/wiki')
 def api_projects_list():
+    '''
+List all projects in the wiki format
+------------------------------------
+    Lists all the projects registered in anitya using the format of the
+    old wiki page.
+
+    ::
+
+        /api/projects/wiki
+
+    Accept GET queries only.
+
+    Sample response:
+
+    ::
+
+      * 2ping None http://www.finnie.org/software/2ping
+      * 3proxy None http://www.3proxy.ru/download/
+    '''
+
 
     project_objs = anitya.lib.model.Project.all(SESSION)
 
@@ -60,6 +134,34 @@ def api_projects_list():
 @APP.route('/api/projects/names/')
 @APP.route('/api/projects/names')
 def api_projects_names():
+    '''
+List all projects names
+------------------------
+    Lists the names of all the projects registered in anitya.
+
+    ::
+
+        /api/projects/names
+
+        /api/projects/names/?pattern=<pattern>
+
+    Accept GET queries only.
+
+    :kwarg pattern: pattern to use to restrict the list of names returned.
+
+    Sample response:
+
+    ::
+
+      {
+        "projects": [
+          "2ping",
+          "3proxy",
+        ],
+        "total": 2
+      }
+
+    '''
 
     pattern = flask.request.args.get('pattern', None)
 
@@ -86,6 +188,46 @@ def api_projects_names():
 
 @APP.route('/api/version/', methods=['POST'])
 def api_get_version():
+    '''
+Retrieve version
+----------------
+    Retrieves the version of a specific project by actually calling its
+    upstream.
+
+    ::
+
+        /api/version/
+
+    Accept POST queries only.
+
+    :arg id: the identifier of the project in anitya.
+
+    Sample response:
+
+    ::
+
+      {
+        "backend": "Sourceforge",
+        "created_on": 1409917222.0,
+        "homepage": "http://sourceforge.net/projects/zero-install",
+        "id": 1,
+        "name": "zero-install",
+        "packages": [
+          {
+            "distro": "Fedora",
+            "package_name": "0install"
+          }
+        ],
+        "regex": "",
+        "updated_on": 1413794215.0,
+        "version": "2.7",
+        "version_url": "0install",
+        "versions": [
+          "2.7"
+        ]
+      }
+
+    '''
 
     project_id = flask.request.form.get('id', None)
     httpcode = 200
@@ -107,7 +249,7 @@ def api_get_version():
         else:
             try:
                 anitya.check_release(project, SESSION)
-                output = project.__json__()
+                output = project.__json__(detailed=True)
             except anitya.lib.exceptions.AnityaException as err:
                 output = {'output': 'notok', 'error': [str(err)]}
                 httpcode = 400
@@ -120,6 +262,45 @@ def api_get_version():
 @APP.route('/api/project/<project_id>/', methods=['GET'])
 @APP.route('/api/project/<project_id>', methods=['GET'])
 def api_get_project(project_id):
+    '''
+Retrieve a specific project
+----------------------------
+    Retrieves a specific project using its identifier in anitya.
+
+    ::
+
+        /api/project/<project_id>
+
+    Accept GET queries only.
+
+    :arg project_id: the identifier of the project in anitya.
+
+    Sample response:
+
+    ::
+
+      {
+        "backend": "custom",
+        "created_on": 1409917222.0,
+        "homepage": "http://www.finnie.org/software/2ping/",
+        "id": 2,
+        "name": "2ping",
+        "packages": [
+          {
+            "distro": "Fedora",
+            "package_name": "2ping"
+          }
+        ],
+        "regex": null,
+        "updated_on": 1414400794.0,
+        "version": "2.1.1",
+        "version_url": "http://www.finnie.org/software/2ping",
+        "versions": [
+          "2.1.1"
+        ]
+      }
+
+    '''
 
     project = anitya.lib.model.Project.get(SESSION, project_id=project_id)
 
@@ -138,6 +319,47 @@ def api_get_project(project_id):
 @APP.route('/api/project/<distro>/<package_name>/', methods=['GET'])
 @APP.route('/api/project/<distro>/<package_name>', methods=['GET'])
 def api_get_project_distro(distro, package_name):
+    '''
+Retrieve project in a distro
+----------------------------
+    Retrieves a project in a distribution via the name of the distribution
+    and the name of the package in said distribution.
+
+    ::
+
+        /api/project/<distro>/<package_name>
+
+    Accept GET queries only.
+
+    :arg distro: the name of the distribution (case insensitive).
+    :arg package_name: the name of the package in the distribution specified.
+
+    Sample response:
+
+    ::
+
+      {
+        "backend": "custom",
+        "created_on": 1409917222.0,
+        "homepage": "http://www.finnie.org/software/2ping/",
+        "id": 2,
+        "name": "2ping",
+        "packages": [
+          {
+            "distro": "Fedora",
+            "package_name": "2ping"
+          }
+        ],
+        "regex": null,
+        "updated_on": 1414400794.0,
+        "version": "2.1.1",
+        "version_url": "http://www.finnie.org/software/2ping",
+        "versions": [
+          "2.1.1"
+        ]
+      }
+
+    '''
 
     package = anitya.lib.model.Packages.by_package_name_distro(
         SESSION, package_name, distro)
@@ -153,7 +375,7 @@ def api_get_project_distro(distro, package_name):
         project = anitya.lib.model.Project.get(
             SESSION, project_id=package.project.id)
 
-        output = project.__json__()
+        output = project.__json__(detailed=True)
         httpcode = 200
 
     jsonout = flask.jsonify(output)
