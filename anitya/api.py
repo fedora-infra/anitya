3f62621dc13b6568df7cb2bb2b909cd2efeedf1c
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-11-07 16:10:15 +0100

    Use bootstrap instead of a local icon to identify the anchors

--- anitya/api.py
+++ anitya/api.py
@@ -22,12 +22,10 @@ def insert_div(content):
     for row in content:
         if row.startswith('<h1 class="title">'):
           title = row.split('"title">')[1].split('</h1>')[0]
-          link = '<a name="%(title)s" class="headerlink" '\
-              'title="Permalink to this headline" href="#%(title)s">'\
-              '<img src="%(url)s" /></a>' % (
+          link = '<a name="%(title)s" class="glyphicon glyphicon-link btn-xs" '\
+              'title="Permalink to this headline" href="#%(title)s"></a>' % (
                   {
                       'title': title.replace(' ', '_'),
-                      'url': flask.url_for('static', filename='link.png'),
                   }
               )
           row = str(row).replace('</h1>', link + '</h1>')
