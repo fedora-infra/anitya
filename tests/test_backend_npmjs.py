c79dc8599209cdcf165526f7ae4bfa08598aca55
Ralph Bean <rbean@redhat.com>
2014-11-30 21:46:08 -0500

    Fix tests.

--- tests/test_backend_npmjs.py
+++ tests/test_backend_npmjs.py
@@ -84,7 +84,7 @@ class NpmjsBackendtests(Modeltests):
         """ Test the get_version function of the npmjs backend. """
         pid = 1
         project = model.Project.get(self.session, pid)
-        exp = '2.48.0'
+        exp = '2.49.0'
         obs = backend.NpmjsBackend.get_version(project)
         self.assertEqual(obs, exp)
 
@@ -120,7 +120,7 @@ class NpmjsBackendtests(Modeltests):
             '2.27.0', '2.28.0', '2.29.0', '2.30.0', '2.31.0', '2.32.0',
             '2.33.0', '2.34.0', '2.35.0', '2.36.0', '2.37.0', '2.38.0',
             '2.39.0', '2.40.0', '2.41.0', '2.42.0', '2.43.0', '2.44.0',
-            '2.45.0', '2.46.0', '2.47.0', '2.48.0',
+            '2.45.0', '2.46.0', '2.47.0', '2.48.0', '2.49.0',
         ]
         obs = backend.NpmjsBackend.get_ordered_versions(project)
         self.assertEqual(obs, exp)
