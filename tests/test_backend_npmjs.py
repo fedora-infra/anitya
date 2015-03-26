dbfaf13d1cb966ac438b80c42ec04b074cd4f2da
Ralph Bean <rbean@redhat.com>
2015-03-25 22:31:08 -0400

    Update tests for a new npm release.

--- tests/test_backend_npmjs.py
+++ tests/test_backend_npmjs.py
@@ -84,7 +84,7 @@ class NpmjsBackendtests(Modeltests):
         """ Test the get_version function of the npmjs backend. """
         pid = 1
         project = model.Project.get(self.session, pid)
-        exp = '2.53.0'
+        exp = '2.54.0'
         obs = backend.NpmjsBackend.get_version(project)
         self.assertEqual(obs, exp)
 
@@ -121,7 +121,7 @@ class NpmjsBackendtests(Modeltests):
             '2.33.0', '2.34.0', '2.35.0', '2.36.0', '2.37.0', '2.38.0',
             '2.39.0', '2.40.0', '2.41.0', '2.42.0', '2.43.0', '2.44.0',
             '2.45.0', '2.46.0', '2.47.0', '2.48.0', '2.49.0', '2.50.0',
-            '2.51.0', '2.52.0', '2.53.0',
+            '2.51.0', '2.52.0', '2.53.0', '2.54.0',
         ]
         obs = backend.NpmjsBackend.get_ordered_versions(project)
         self.assertEqual(obs, exp)
