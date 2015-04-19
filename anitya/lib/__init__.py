1514fb1db87978df1a9b72935da5756c8f43a4cc
David Gay <dgay@redhat.com>
2015-04-19 13:54:13 -0700

    use pingou's sexier state check

--- anitya/lib/__init__.py
+++ anitya/lib/__init__.py
@@ -309,26 +309,26 @@ def set_flag_state(session, flag, state, user_mail):
 
     # Don't toggle the state or send a new fedmsg if the flag's
     # state wouldn't actually be changed.
-    if flag.state != state:
+    flag.state == state: return
 
-        flag.state = state
-        session.add(flag)
+    flag.state = state
+    session.add(flag)
 
-        try:
-            session.flush()
-        except SQLAlchemyError as err:
-            log.exception(err)
-            session.rollback()
-            raise anitya.lib.exceptions.AnityaException(
-                'Could not set the state of this flag.')
+    try:
+        session.flush()
+    except SQLAlchemyError as err:
+        log.exception(err)
+        session.rollback()
+        raise anitya.lib.exceptions.AnityaException(
+            'Could not set the state of this flag.')
 
-        anitya.log(
-            session,
-            topic='project.flag.set',
-            message=dict(
-                agent=user_mail,
-                flag=flag.id,
-                state=state,
-            )
+    anitya.log(
+        session,
+        topic='project.flag.set',
+        message=dict(
+            agent=user_mail,
+            flag=flag.id,
+            state=state,
         )
-        session.commit()
+    )
+    session.commit()
