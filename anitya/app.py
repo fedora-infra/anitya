0d8934f1d89ae5651a8f843445186b1f535970c1
David Gay <dgay@redhat.com>
2015-05-11 13:55:08 -0700

    use fancy new bootstrap flash messages

--- anitya/app.py
+++ anitya/app.py
@@ -75,11 +75,28 @@ def format_examples(examples):
     return output
 
 
+@APP.template_filter('context_class')
+def context_class(category):
+    ''' Return bootstrap context class for a given category. '''
+    values = {
+        'message': 'default',
+        'error': 'danger',
+        'info': 'info',
+    }
+    return values.get(category, 'warning')
+
+
 @APP.before_request
 def check_auth():
     ''' Set the flask.g variables using the session information if the user
     is logged in.
     '''
+
+    flask.flash('default')
+    flask.flash('error, etc..', 'error')
+    flask.flash('warning', 'warning')
+    flask.flash('info', 'info')
+
     flask.g.auth = Bunch(
         logged_in=False,
         method=None,
