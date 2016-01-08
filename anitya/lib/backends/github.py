23a1513cf3adcc754e05ac5aa090509923e59c0e
Chaoyi Zha <summermontreal@gmail.com>
2016-01-08 18:25:10 -0500

    Fix GitHub upstream: fix issues with detecting URL, fix issues with creating releases URL

--- anitya/lib/backends/github.py
+++ anitya/lib/backends/github.py
@@ -28,6 +28,20 @@ class GithubBackend(BaseBackend):
         'https://github.com/fedora-infra/pkgdb2',
     ]
 
+    def get_url_from_regex_match(cls, regex_match):
+        url_template = 'https://github.com/{}/tags'
+
+        repo_owner = regex_match.group(1)
+        repo_name = regex_match.group(2)
+
+        url = url_template.format(
+            "{}/{}".format(
+                repo_owner, repo_name
+            )
+        )
+
+        return url
+
     @classmethod
     def get_version(cls, project):
         ''' Method called to retrieve the latest version of the projects
@@ -59,15 +73,26 @@ class GithubBackend(BaseBackend):
             when the versions cannot be retrieved correctly
 
         '''
+        github_repo_regex = 'http[s]?:\/\/github\.com\/([^\/]+)\/([^\/]+)[\/]?'
+
+        homepage_gh_regex_match = re.match(github_repo_regex, project.homepage)
+
         if project.version_url:
-            url_template = 'https://github.com/%(version_url)s/tags'
-            version_url = project.version_url.replace('https://github.com/', '')
-            url = url_template % {'version_url': version_url}
-        elif project.homepage.startswith('https://github.com'):
-            url = project.homepage
-            if url.endswith('/'):
-                url = project.homepage[:1]
-            url += '/tags'
+            matched_url_regex = re.match(github_repo_regex, project.version_url)
+            if matched_url_regex:
+                # matches github repo url
+                # e.g https://github.com/fedora-infra/anitya
+                url = cls.get_url_from_regex_match(matched_url_regex)
+            else:
+                # does not match github repo url,
+                # assume repoowner/reponame format
+                # e.g fedora-infra/anitya
+                url = url_template.format(project.version_url)
+
+        elif homepage_gh_regex_match:
+            # homepage matches github repo regex
+            url = cls.get_url_from_regex_match(homepage_gh_regex_match)
+
         else:
             raise AnityaPluginException(
                 'Project %s was incorrectly set-up' % project.name)
