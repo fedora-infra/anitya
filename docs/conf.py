b4ade5abc569e37e3ebd78ac312a27750d199293
Jeremy Cline <jeremy@jcline.org>
2016-12-21 13:13:23 -0500

    Remove the githubpages sphinx extension
    We don't use githubpages.
    
    Signed-off-by: Jeremy Cline <jeremy@jcline.org>

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
