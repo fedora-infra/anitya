94f316c541fc6db1fc95b17fd2ffba820c6f4741
Nick Coghlan <ncoghlan@gmail.com>
2017-02-28 16:28:11 +1000

    Explain double-rendering problem for 401 errors
    There's a problematic interaction between flask-oidc
    and Flask-RESTful that is currently leading to 401
    error responses being double rendered as JSON strings
    rather than being JSON mappings as expected.

--- anitya/authentication.py
+++ anitya/authentication.py
@@ -127,6 +127,8 @@ def require_api_token(*scopes):
 
 
 def _report_oidc_not_configured(*args, **kwds):
+    # TODO: Avoid double-rendering to JSON when used with Flask-RESTful
+    #       https://github.com/release-monitoring/anitya/issues/443
     error_details = json.dumps({
         'error': 'oidc_not_configured',
         'error_description': 'OpenID Connect is not configured on the server'
