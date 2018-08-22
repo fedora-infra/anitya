64e4bdeee2c391c6388a3d5b10657e64c3e4068c
Michal Konečný <mkonecny@redhat.com>
2018-08-22 15:19:35 +0200

    Add GitLab API backend

---
+++ anitya/lib/backends/gitlab.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2018  Red Hat, Inc.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions
+# of the GNU General Public License v.2, or (at your option) any later
+# version.  This program is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY expressed or implied, including the
+# implied warranties of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.  You
+# should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# Any Red Hat trademarks that are incorporated in the source
+# code or documentation are not subject to the GNU General Public
+# License and may only be used or replicated with the express permission
+# of Red Hat, Inc.
+#
+"""
+ Authors:
+   Michal Konecny <mkonecny@redhat.com>
+
+"""
+
+from anitya.lib.backends import BaseBackend
+from anitya.lib.exceptions import AnityaPluginException
+
+import logging
+
+_log = logging.getLogger(__name__)
+
+
+class GitlabBackend(BaseBackend):
+    ''' The custom class for projects hosted on gitlab.
+
+    This backend allows to specify a version_url and owner, name of the repository.
+    '''
+
+    name = 'GitLab'
+    examples = [
+        'https://gitlab.gnome.org/GNOME/gnome-video-arcade',
+        'https://gitlab.com/xonotic/xonotic',
+    ]
+
+    @classmethod
+    def get_version(cls, project):
+        ''' Method called to retrieve the latest version of the projects
+        provided, project that relies on the backend of this plugin.
+
+        :arg Project project: a :class:`anitya.db.models.Project` object whose backend
+            corresponds to the current plugin.
+        :return: the latest version found upstream
+        :return type: str
+        :raise AnityaPluginException: a
+            :class:`anitya.lib.exceptions.AnityaPluginException` exception
+            when the version cannot be retrieved correctly
+
+        '''
+        return cls.get_ordered_versions(project)[-1]
+
+    @classmethod
+    def get_versions(cls, project):
+        ''' Method called to retrieve all the versions (that can be found)
+        of the projects provided, project that relies on the backend of
+        this plugin.
+
+        :arg Project project: a :class:`anitya.db.models.Project` object whose backend
+            corresponds to the current plugin.
+        :return: a list of all the possible releases found
+        :return type: list
+        :raise AnityaPluginException: a
+            :class:`anitya.lib.exceptions.AnityaPluginException` exception
+            when the versions cannot be retrieved correctly
+
+        '''
+        url_template = '%(hostname)s/api/v4/projects/%(owner)s%%2F%(repo)s/repository/tags'
+        if project.version_url:
+            tokens = project.version_url.split('/')
+        elif project.homepage:
+            tokens = project.homepage.split('/')
+        else:
+            raise AnityaPluginException(
+                'Project %s was incorrectly set-up' % project.name)
+
+        url = url_template % {'hostname': tokens[0] + '//' + tokens[2],
+                              'owner': tokens[3],
+                              'repo': tokens[4]}
+
+        resp = cls.call_url(url)
+
+        if resp.ok:
+            json = resp.json()
+        else:
+            raise AnityaPluginException(
+                '%s: Server responded with status "%s": "%s"' % (
+                    project.name, resp.status_code, resp.reason))
+
+        _log.debug('Received %d tags for %s' % (len(json), project.name))
+
+        tags = []
+        for tag in json:
+            tags.append(tag["name"])
+
+        return tags
