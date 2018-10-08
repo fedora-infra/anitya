0ecdf9499d628a53c2c014d2e4b7ce31e1d0bb6b
Eli Young <elyscape@gmail.com>
2018-10-08 12:37:20 -0700

    Populate missing GitHub owner/project pairs
    This uses the homepage field to populate any null version_url fields on
    projects that use the GitHub backend.
    
    Signed-off-by: Eli Young <elyscape@gmail.com>

---
+++ anitya/db/migrations/versions/7a8c4aa92678_add_missing_github_owner_project_pairs.py
@@ -0,0 +1,36 @@
+"""Add missing GitHub owner/project pairs
+
+Revision ID: 7a8c4aa92678
+Revises: 540bdcf7edbc
+Create Date: 2018-10-08 12:25:07.006729
+"""
+
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = '7a8c4aa92678'
+down_revision = '540bdcf7edbc'
+
+
+def upgrade():
+    """Populate missing GitHub owner/project pairs from homepage."""
+    op.execute("""
+        UPDATE projects
+        SET version_url=trim(substr(trim(homepage), 19), '/')
+        WHERE backend = 'GitHub'
+            AND trim(homepage) LIKE 'http://github.com/%'
+            AND (version_url IS NULL OR version_url = '')
+    """)
+    op.execute("""
+        UPDATE projects
+        SET version_url=trim(substr(trim(homepage), 20), '/')
+        WHERE backend = 'GitHub'
+            AND trim(homepage) LIKE 'https://github.com/%'
+            AND (version_url IS NULL OR version_url = '')
+    """)
+
+
+def downgrade():
+    """No-op, as empty version_url wouldn't have worked before anyway."""
+    raise NotImplementedError
