056a624ca6491437e47d5d3725d95fe488b57222
Eli Young <elyscape@gmail.com>
2018-10-08 12:18:48 -0700

    Add migration for new GitHub backend
    The new GitHub backend expects that version_url will be an owner/project
    pair rather than a URL.
    
    Signed-off-by: Eli Young <elyscape@gmail.com>

---
+++ anitya/db/migrations/versions/540bdcf7edbc_convert_github_url_to_owner_project.py
@@ -0,0 +1,32 @@
+"""Convert GitHub URL to owner/project
+
+Revision ID: 540bdcf7edbc
+Revises: 27342bce1d0f
+Create Date: 2018-10-08 11:23:33.361838
+"""
+
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = '540bdcf7edbc'
+down_revision = '27342bce1d0f'
+
+
+def upgrade():
+    """Convert GitHub URL to owner/project to work with the new GitHub backend."""
+    op.execute("""
+        UPDATE projects
+        SET version_url=trim(substr(version_url, 19), '/')
+        WHERE backend = 'GitHub' AND version_url LIKE 'http://github.com/%'
+    """)
+    op.execute("""
+        UPDATE projects
+        SET version_url=trim(substr(version_url, 20), '/')
+        WHERE backend = 'GitHub' AND version_url LIKE 'https://github.com/%'
+    """)
+
+
+def downgrade():
+    """No-op, as owner/project works fine with the old GitHub backend."""
+    raise NotImplementedError
