67b737ec83072652df8f4e7f41f123a6c3c369e2
Michal Konečný <mkonecny@redhat.com>
2018-11-06 09:29:01 +0100

    Fix: Multiple heads in alembic
    Signed-off-by: Michal Konečný <mkonecny@redhat.com>

---
+++ anitya/db/migrations/versions/ac10bf3f974c_.py
@@ -0,0 +1,18 @@
+"""empty message
+
+Revision ID: ac10bf3f974c
+Revises: ('c8735fa14a0a', '7a8c4aa92678')
+Create Date: 2018-11-06 08:27:37.278677
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'ac10bf3f974c'
+down_revision = ('c8735fa14a0a', '7a8c4aa92678')
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
