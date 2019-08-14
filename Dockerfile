7fab829f031bb9f67601a3e5e116161500047e74
Anatoli Babenia <anatoli@rainforce.org>
2019-08-14 22:11:41 +0300

    docker: Hacktober work towards Anitya containerization
     * Update to F32 (on par with Vagrant)
     * Run docker image on Travis
     * Replace SECRET_KEY in Docker image on DB init
    
    Replace `secret_key` if the config already exists,
    and create config if there is no such file yet.
    
     * Anitya source is in /app
    
    https://docs.docker.com/develop/develop-images/dockerfile_best-practices/
    
     * Rip off Apache and use Flask
     * Reuse Ansible role for installing packages in Dockerfile
     * ansible: Make location of Anitya src configurable

--- Dockerfile
+++ Dockerfile
@@ -1,32 +1,17 @@
-FROM fedora
-FROM fedora:24
-ENV ANITYA_WEB_CONFIG /etc/anitya/config.py
-# Updating libnghttp2 is required in Fedora 24
-RUN dnf update -y libnghttp2 && \
-    dnf install -y \
-        python-flask \
-        python-flask-wtf \
-        python-wtforms \
-        python-dateutil \
-        python-straight-plugin \
-        python-setuptools \
-        python-sqlalchemy \
-        python-psycopg2 \
-        python-mysql \
-        fedmsg \
-        httpd \
-        mod_wsgi && \
-    dnf autoremove -y && \
+FROM fedora:32
+
+COPY . /app
+WORKDIR /app
+
+RUN dnf install -y ansible
+# Reuse Ansible playbooks for installing packages
+RUN ansible-playbook ansible/docker-playbook.yml --tags packages,install
+RUN dnf autoremove -y && \
     dnf clean all -y
 
-COPY ./ /opt/anitya/src
-WORKDIR /opt/anitya/src
 
 RUN mkdir /etc/anitya
-RUN rm -f /etc/httpd/conf.d/welcome.conf
-RUN cp /opt/anitya/src/files/docker_apache.conf /etc/httpd/conf.d/anitya.conf
-RUN chown -R apache:apache /opt/anitya
-RUN chmod +x /opt/anitya/src/files/docker.sh
+RUN chmod +x /app/files/docker.sh
 
 EXPOSE 80
-ENTRYPOINT /opt/anitya/src/files/docker.sh
+ENTRYPOINT /app/files/docker.sh
--- files/docker.sh
+++ files/docker.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
-# Remove any remnants of apache if it didn't shut down properly
-rm -f /var/run/httpd/httpd.pid
 
 # This if statement should only run on the first time the container is spun up
 if [ ! -f /.db_created ]; then
 
-    if [ -z "${DB_URL}" ]; then
-        echo 'The DB_URL environment variable was not set. This value is required.'
-        exit 1
+    if [ -z "$SECRET_KEY" ]; then
+        SECRET_KEY=$(tr -dc 'a-f0-9' < /dev/urandom | fold -w 24 | head -n 1)
     fi
 
-    if [ "${SECRET_KEY}" ]; then
-        echo "SECRET_KEY = '${SECRET_KEY}'" > /etc/anitya/config.py
+    CONFIG_FILE=/etc/anitya/config.toml
+    if [ -f "$CONFIG_FILE" ]; then
+        echo "Replacing SECRET_KEY with generated value"
+        sed -i "s/changeme please/${SECRET_KEY}/g" "$CONFIG_FILE"
     else
-        echo "SECRET_KEY = '$(< /dev/urandom tr -dc 'a-f0-9' | fold -w 24 | head -n 1)'" > /etc/anitya/config.py
+        echo "secret_key = '${SECRET_KEY}'" > "$CONFIG_FILE"
     fi
 
-    echo "DB_URL = '${DB_URL}'" >> /etc/anitya/config.py
-    python /opt/anitya/src/createdb.py
+    python3 /app/createdb.py
     touch /.db_created
 fi
 
-unset DB_URL
-exec /usr/sbin/httpd -D FOREGROUND
+exec env FLASK_APP=runserver.py flask run --host 0.0.0.0 --port 80
