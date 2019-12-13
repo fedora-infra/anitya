5807f3549616e7dd0f77b3d0d096ac33e7c5591a
Nils Philippsen <nils@redhat.com>
2019-12-13 12:08:16 +0100

    Add Project.latest_known_cursor
    This lets us store e.g. GitHub API v4 cursors for the latest known
    release of a project, in order to reduce the number of API calls we have
    to perform.
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

---
+++ anitya/db/migrations/versions/92a2e9eba0a7_add_project_latest_known_cursor.py
@@ -0,0 +1,25 @@
+"""add Project.latest_known_cursor
+
+Revision ID: 92a2e9eba0a7
+Revises: 5e209766aead
+Create Date: 2019-12-12 10:11:38.652950
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "92a2e9eba0a7"
+down_revision = "5e209766aead"
+
+
+def upgrade():
+    op.add_column(
+        "projects",
+        sa.Column("latest_known_cursor", sa.String(length=200), nullable=True),
+    )
+
+
+def downgrade():
+    op.drop_column("projects", "latest_known_cursor")
