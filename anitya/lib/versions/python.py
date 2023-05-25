b6ce1b1357096a840a239dea99ec220cd0195eb0
Michal Konečný <mkonecny@redhat.com>
2023-05-25 13:33:55 +0200

    Rewrite the Python versioning scheme implementation
    The current solution didn't work well with Anitya and introduced plenty of new
    issues. This rewrite tries to solve all the issues and hopefully not creating
    new ones.
    
    Fixes #1586
    
    Signed-off-by: Michal Konečný <mkonecny@redhat.com>

--- anitya/lib/versions/python.py
+++ anitya/lib/versions/python.py
@@ -29,7 +29,9 @@ See `Version scheme` in PEP 440.
    https://pypi.org/
 """
 
+from datetime import datetime
 import functools
+from typing import Optional
 
 # Import entire modules so it's clear which "Version" and "InvalidVersion"
 # is used
@@ -44,7 +46,36 @@ class PythonVersion(base.Version):
 
     name = "Python (PEP 440)"
 
-    def version_object(self):
+    def __init__(
+        self,
+        version: Optional[str] = None,
+        prefix: Optional[str] = None,
+        created_on: Optional[datetime] = None,
+        pattern: Optional[str] = None,
+        cursor: Optional[str] = None,
+        commit_url: Optional[str] = None,
+        pre_release_filter: Optional[str] = None,
+    ):
+        """
+        Constructor of Version class.
+
+        Params:
+            version: Raw version
+            prefix: Prefix to remove
+            created_on: Date of creation
+            pattern: Calendar version pattern.
+                See `Calendar version scheme_` for more information.
+            cursor: An opaque, backend-specific cursor pointing to the version.
+            commit_url: A URL pointing to the commit tagged as the version.
+            pre_release_filter: A filter used to identify pre-release versions
+        """
+        super(PythonVersion, self).__init__(
+            version, prefix, created_on, pattern, cursor, commit_url, pre_release_filter
+        )
+
+        self.version_object = self.get_version_object()
+
+    def get_version_object(self):
         """
         Parse the version string to an object representing the version.
 
@@ -61,40 +92,90 @@ class PythonVersion(base.Version):
         # The result should be usable with packaging.version
         try:
             return packaging.version.Version(version)
-        except packaging.version.InvalidVersion as e:
-            raise base.InvalidVersion(str(e)) from e
+        except packaging.version.InvalidVersion:
+            return None
 
     def parse(self):
         """
-        Return string representation of the version object
+        Parse the version string and returns something usable by Anitya.
+
+        Returns:
+          str: The version string processed by `packaging` module.
         """
-        try:
-            return str(self.version_object())
-        except base.InvalidVersion:
-            # When the version can't be validated by packaging.version module just return the string
+        if self.version_object:
+            return str(self.version_object)
+        else:
             return super().parse()
 
     def prerelease(self):
         """
         Check this is a pre-release version.
         """
-        try:
-            ver_obj = self.version_object()
-        except base.InvalidVersion:
+        if not self.version_object:
             return False
 
         for pre_release_filter in self.pre_release_filters:
             if pre_release_filter and pre_release_filter in self.version:
                 return True
 
-        return ver_obj.is_prerelease
+        return self.version_object.is_prerelease
 
     def postrelease(self):
         """
         Check this is a post-release version.
         """
-        try:
-            ver_obj = self.version_object()
-        except base.InvalidVersion:
+        if not self.version_object:
             return False
-        return ver_obj.is_postrelease
+
+        return self.version_object.is_postrelease
+
+    def newer(self, other_versions):
+        """
+        Check a version against a list of other versions to see if it's newer.
+
+        Example:
+            >>> version = Version(version='1.1.0')
+            >>> version.newer([Version(version='1.0.0')])
+            True
+            >>> version.newer(['1.0.0', '0.0.1'])  # You can pass strings!
+            True
+            >>> version.newer(['1.2.0', '2.0.1'])
+            False
+
+        Args:
+            other_versions (list): A list of version strings or Version
+                objects to check the `version` string against.
+        Returns:
+            bool: True if self is the newest version, ``False otherwise``.
+        Raises:
+            InvalidVersion: if one or more of the version
+                strings provided cannot be parsed.
+        """
+        if isinstance(other_versions, (base.Version, str)):
+            other_versions = [other_versions]
+        cast_versions = []
+        for version in other_versions:
+            if not isinstance(version, type(self)):
+                version = type(self)(version=version)
+            cast_versions.append(version)
+        return all([self.version_object > v.version_object for v in cast_versions])
+
+    def __lt__(self, other):
+        """Support < comparison via objects returned from :meth:`version_object`"""
+        # Handle the cases where one or both can't be validated. Validated versions
+        # always sort higher than unvalidated versions.
+        if not self.version_object and not other.version_object:
+            return self.version.__lt__(other.version)
+        if not other.version_object:
+            return False
+        if not self.version_object:
+            return True
+
+        return self.version_object.__lt__(other.version_object)
+
+    def __eq__(self, other):
+        """Support == comparison via objects returned from :meth:`version_object`"""
+        # Handle the cases where one or both can't be validated.
+        if not self.version_object or not other.version_object:
+            return self.version.__eq__(other.version)
+        return self.version_object.__eq__(other.version_object)
