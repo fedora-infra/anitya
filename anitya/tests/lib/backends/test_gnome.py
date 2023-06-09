bdf202a990e3c8f8b87118d95ca63efa15aed4f9
Liora Milbaum <liora@lmb.co.il>
2023-06-09 11:36:01 +0300

    pylint anitya/tests/lib/backends/test_gnome.py

--- anitya/tests/lib/backends/test_gnome.py
+++ anitya/tests/lib/backends/test_gnome.py
@@ -38,7 +38,7 @@ class GnomeBackendtests(DatabaseTestCase):
 
     def setUp(self):
         """Set up the environnment, ran before every tests."""
-        super(GnomeBackendtests, self).setUp()
+        super().setUp()
 
         create_distro(self.session)
         self.create_project()
