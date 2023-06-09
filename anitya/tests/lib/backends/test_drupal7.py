a467dd84f0d211e4c50c2de7696c3ce2adb5d105
Liora Milbaum <liora@lmb.co.il>
2023-06-09 22:08:53 +0300

    pylint anitya/tests/lib/backends/test_drupal7.py

--- anitya/tests/lib/backends/test_drupal7.py
+++ anitya/tests/lib/backends/test_drupal7.py
@@ -38,7 +38,7 @@ class Drupal7Backendtests(DatabaseTestCase):
 
     def setUp(self):
         """Set up the environnment, ran before every tests."""
-        super(Drupal7Backendtests, self).setUp()
+        super().setUp()
 
         create_distro(self.session)
         self.create_project()
