286f0efd8daf9c4b6d86140980537a8f2d37109f
Sheikh Limon <sheikhlimondev@gmail.com>
2026-09-12 23:29:08 +0600

    Fix duplicate /gnu/ path in GNU backend version check
    Fixes #2073
    
    Signed-off-by: Sheikh Limon <sheikhlimondev@gmail.com>

--- anitya/lib/backends/gnu.py
+++ anitya/lib/backends/gnu.py
@@ -24,7 +24,7 @@ class GnuBackend(BaseBackend):
     """
 
     name = "GNU project"
-    examples = ["https://ftpmirror.gnu.org/pub/gnu/gnash/"]
+    examples = ["https://ftpmirror.gnu.org/gnash/"]
 
     @classmethod
     def get_version_url(cls, project):
@@ -38,7 +38,7 @@ class GnuBackend(BaseBackend):
         Returns:
             str: url used for version checking
         """
-        url = f"https://ftpmirror.gnu.org/gnu/{project.name}/"  # noqa: E231
+        url = f"https://ftpmirror.gnu.org/{project.name}/"  # noqa: E231
 
         return url
 
